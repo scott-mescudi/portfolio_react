dade1a462290a38901f7c939453088dcc516f88d
script kiddie <anish.chittu2007@gmail.com>
2025-01-02 05:44:38 +0100

    feat: add widget components for various technologies and integrate them into the Projects page

---
+++ src/components/Widgets.jsx
@@ -0,0 +1,37 @@
+export function ReactWidget() {
+    return (
+        <div className="h-12 w-14 bg-neutral-900 rounded-lg"><img className="p-1 items-center justify-center h-full w-full" src="/logos/react.svg"></img></div>
+    )
+}
+
+export function GoWidget() {
+    return (
+        <div className="h-12 w-14 bg-neutral-900 rounded-lg"><img className="p-1 items-center justify-center h-full w-full" src="/logos/go.svg"></img></div>
+    )
+}
+
+export function TailwindWidget() {
+    return (
+        <div className="h-12 w-14 bg-neutral-900 rounded-lg"><img className="p-1 items-center justify-center h-full w-full" src="/logos/tailwind.svg"></img></div>
+    )
+}
+
+export function DockerWidget() {
+    return (
+        <div className="h-12 w-14 bg-neutral-900 rounded-lg"><img className="p-1 items-center justify-center h-full w-full" src="/logos/docker.svg"></img></div>
+    )
+}
+
+export function CWidget() {
+    return (
+        <div className="h-12 w-14 bg-neutral-900 rounded-lg"><img className="p-1 items-center justify-center h-full w-full" src="/logos/c.svg"></img></div>
+    )
+}
+
+export function TypescriptWidget() {
+    return (
+        <div className="h-12 w-14 bg-neutral-900 rounded-lg"><img className="p-1 items-center justify-center h-full w-full" src="/logos/typescript.svg"></img></div>
+    )
+}
+
+
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,40 +1,58 @@
+import { GoWidget, ReactWidget, TailwindWidget } from "../../components/Widgets";
+
 const DefaultProjects = [
-    {title: "Stegano", description: "A blazing-fast steganography library for Go, designed for seamless data concealment with high performance and reliability.", image: "stegano.png", url: "/projects/stegano"},
-    {title: "Stegano", description: "A blazing-fast steganography library for Go, designed for seamless data concealment with high performance and reliability.", image: "stegano.png", url: "/projects/stegano"},
-    {title: "Stegano", description: "A blazing-fast steganography library for Go, designed for seamless data concealment with high performance and reliability.", image: "stegano.png", url: "/projects/stegano"},
-    {title: "Stegano", description: "A blazing-fast steganography library for Go, designed for seamless data concealment with high performance and reliability.", image: "stegano.png", url: "/projects/stegano"},
-    {title: "Stegano", description: "A blazing-fast steganography library for Go, designed for seamless data concealment with high performance and reliability.", image: "stegano.png", url: "/projects/stegano"},
+    { 
+        title: "Stegano", 
+        description: "A blazing-fast steganography library for Go, designed for seamless data concealment with high performance and reliability.", 
+        image: "stegano.svg", 
+        url: "",
+        widgets: [<GoWidget />] 
+    },
+    { 
+        title: "Portfolio Website", 
+        description: "This site, a personal portfolio website to showcase my skills and projects.", 
+        image: "website.png", 
+        url: "", 
+        widgets: [
+            <ReactWidget key="react" />,
+            <TailwindWidget key="tailwind" />
+        ] 
+    },
 
-]
+];
 
+// there is something wrong with sizing, its not consistent. need to fix URGENT!!!!
 
 export function Projects({projects = DefaultProjects}) {
     return (
-        <div className="min-h-screen my-5 w-full flex items-center flex-col ">
+        <>
+        <div className="min-h-screen my-5 w-full flex items-center flex-col">
             <h1 className="w-6/12 text-white text-center font-semibold sm:text-4xl">NOTABLE PROJECTS</h1>
-            <div className="flex flex-col mt-7 w-6/12 gap-5 ">
-            {projects.map((item, idx) => (
-                <a>
-                    <div key={idx} className="w-full bg-neutral-950 rounded-xl overflow-hidden flex flex-row hover:scale-105 ease-in-out duration-200">
-                        <div id="image" className="w-2/5 bg-slate-800">
-                                <img className="w-full h-full object-cover" src="stegano.png"></img>
-                        </div>
-
-                        <div className="flex flex-col w-full">
-                            <div id="text" className="h-4/6 flex flex-col p-4 gill">
-                                <h1 className="text-white text-2xl">{item.title}</h1>
-                                <p className="text-white text-opacity-50 ">{item.description}</p>
+            <div className="flex flex-col mt-7 w-6/12 gap-5">
+                {projects.map((item, idx) => (
+                    <a key={idx} href={item.url}>
+                        <div className="w-full sm:min-h-44 bg-neutral-950 rounded-xl overflow-hidden flex flex-row hover:scale-105 ease-in-out duration-200">
+                            <div id="image" className="w-2/5 bg-neutral-900">
+                                <img className="w-full h-full object-cover" src={item.image} alt="project image" />
                             </div>
 
-                            <div id="tech" className="h-2/6 flex flex-col p-4 gill bg-white">
+                            <div className="flex flex-col w-full">
+                                <div id="text" className="flex-1 flex flex-col p-4 gill">
+                                    <h1 className="text-white text-2xl">{item.title}</h1>
+                                    <p className="text-white text-opacity-50 ">{item.description}</p>
+                                </div>
 
+                                <div id="tech" className="flex-1 flex items-center p-3 gap-3">
+                                        {item.widgets.map((Widget, index) => (
+                                            <div key={index}>{Widget}</div>
+                                        ))}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-            ))}
-
+                    </a>
+                ))}
             </div>
         </div>
-    )
+        </>
+    );
 }
