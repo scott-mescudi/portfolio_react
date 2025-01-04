b8242389c2eb0f5760da26e7c9106f2eff5cd171
script kiddie <anish.chittu2007@gmail.com>
2025-01-04 20:17:29 +0100

    feat: enhance layout and structure of Footer, About, and Home components for improved responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,33 @@
 export function Footer() {
     return (
         <>
-            <footer id="footer" className="h-full overflow-hidden sm:w-1/2 w-4/5 rounded-t-2xl justify-center dark:bg-neutral-950 bg-slate-300 flex bottom-0 gap-2 flex-row sm:px-8 py-5">
-            <ul className="font-mono md:flex sm:flex-col hidden dark:text-white opacity-50 ">
-                <li>Jay</li>
-                <li>Suriname</li>
-                <li>2007</li>
-            </ul>
+            <div className="w-full relative flex justify-center bottom-0">
+                <footer id="footer" className="h-full overflow-hidden sm:w-1/2 w-4/5 rounded-t-2xl justify-center dark:bg-neutral-950 flex bg-slate-300 gap-2 flex-row sm:px-8 py-5">
+                    <ul className="font-mono md:flex sm:flex-col hidden dark:text-white opacity-50 ">
+                        <li>Jay</li>
+                        <li>Suriname</li>
+                        <li>2007</li>
+                    </ul>
 
-             <div className="h-1/12 md:flex w-0.5 sm:ml-7 hidden opacity-40 rounded-md dark:bg-white"></div>
+                    <div className="h-1/12 md:flex w-0.5 sm:ml-7 hidden opacity-40 rounded-md dark:bg-white"></div>
 
-            <ul className="font-mono w-[80px] ml-3 dark:text-white ">
-                <li className="opacity-90 ">Links:</li>
-                <li className="opacity-50 hover:opacity-90 duration-200 ease-in-out "><a target="_blank" href="https://github.com/scott-mescudi">Github.1</a></li>
-                <li className="opacity-50 hover:opacity-90 duration-200 ease-in-out "><a target="_blank" href="https://github.com/gopher93185789">Github.2</a></li>
-            </ul>
+                    <ul className="font-mono w-[80px] ml-3 dark:text-white ">
+                        <li className="opacity-90 ">Links:</li>
+                        <li className="opacity-50 hover:opacity-90 duration-200 ease-in-out "><a target="_blank" href="https://github.com/scott-mescudi">Github.1</a></li>
+                        <li className="opacity-50 hover:opacity-90 duration-200 ease-in-out "><a target="_blank" href="https://github.com/gopher93185789">Github.2</a></li>
+                    </ul>
 
-             <div className="h-1/12 sm:flex w-0.5 sm:ml-7 hidden opacity-40 rounded-md dark:bg-white"></div>
+                    <div className="h-1/12 sm:flex w-0.5 sm:ml-7 hidden opacity-40 rounded-md dark:bg-white"></div>
 
-            <ul className="font-mono ml-3 dark:text-white ">
-                <li className="opacity-90">Pages:</li>
-                <li className="opacity-50 hover:opacity-90 duration-200 ease-in-out"><a href="/home">Home</a></li>
-                <li className="opacity-50 hover:opacity-90 duration-200 ease-in-out"><a href="/about">About</a></li>
-                <li className="opacity-50 hover:opacity-90 duration-200 ease-in-out"><a href="/projects">Projects</a></li>
-            </ul>
-
-            </footer>
+                    <ul className="font-mono ml-3 dark:text-white ">
+                        <li className="opacity-90">Pages:</li>
+                        <li className="opacity-50 hover:opacity-90 duration-200 ease-in-out"><a href="/home">Home</a></li>
+                        <li className="opacity-50 hover:opacity-90 duration-200 ease-in-out"><a href="/about">About</a></li>
+                        <li className="opacity-50 hover:opacity-90 duration-200 ease-in-out"><a href="/projects">Projects</a></li>
+                    </ul>
+                </footer>
+            </div>
+        
         </>
     )
 }
--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -31,22 +31,23 @@ export function About() {
 
 
             <div id="boxes" className="w-full h-full lg:h-screen flex gap-5 flex-col items-center">
-                <div className="sm:w-1/2 w-4/5 h-1/2 mt-5  bg-neutral-950 flex flex-col p-5 rounded-xl">
+                <div className="sm:w-1/2 w-4/5 h-80 sm:h-1/2 mt-5 bg-neutral-950 flex flex-col p-5 rounded-xl">
                         <h1 className="w-full text-white font-semibold sm:text-2xl">Cars</h1>
                         <p className="w-full text-white text-opacity-45">I love tuning and working on cars.</p>
                         <div className="w-full h-full p-5 flex justify-center items-center ">
                             <img decoding="async" loading="lazy" src="gtr.png" className="rounded-md hover:scale-110 duration-500" />
                         </div> 
                 </div>
+                
                 <div className="sm:w-1/2 w-4/5 h-1/2 gap-5 mb-5 flex lg:flex-row flex-col">
-                    <div className="w-full h-full p-5 bg-neutral-950 flex flex-col rounded-xl items-center">
+                    <div className="w-full h-80 sm:h-full p-5 bg-neutral-950 flex flex-col rounded-xl items-center">
                         <h1 className="w-full text-white font-semibold sm:text-2xl">Photography</h1>
                         <p className="w-full text-white text-opacity-45">One of my recent hobbies.</p>
                         <div className="w-full h-full p-5 flex justify-center items-center ">
                             <img decoding="async" loading="lazy" src="go.gif" className="rounded-md py-1 hover:scale-110 duration-500" /> {/* 'fume' remove this for photos from camera with a filter or sum to make it dark */}
                         </div> 
                     </div>
-                    <div className="w-full h-full p-5 bg-neutral-950 flex flex-col rounded-xl items-center">
+                    <div className="w-full h-80 sm:h-full p-5 bg-neutral-950 flex flex-col rounded-xl items-center">
                         <h1 className="w-full text-white font-semibold sm:text-2xl">Software</h1>
                         <p className="w-full text-white text-opacity-45">In my free time, I really enjoy software development. I love building things.</p>
                         <Lines />
