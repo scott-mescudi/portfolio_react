1b3ab9f3bdcb7e52e416f021098e88064fbd41ee
script kiddie <anish.chittu2007@gmail.com>
2025-01-07 08:15:57 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,7 @@ export function Footer() {
                 <footer id="footer" className="h-full overflow-hidden sm:w-1/2 w-4/5 rounded-t-2xl justify-center dark:bg-neutral-950 flex bg-slate-300 gap-2 flex-row sm:px-8 py-5">
                     <ul className="font-mono md:flex sm:flex-col hidden dark:text-white opacity-50 ">
                         <li>Jay</li>
-                        <li>Suriname</li>
-                        <li>2007</li>
+                        <li>sxm</li>
                     </ul>
 
                     <div className="h-1/12 md:flex w-0.5 sm:ml-7 hidden opacity-40 rounded-md dark:bg-white"></div>
