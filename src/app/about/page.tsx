3089d84b480b64603dee72cc75212d4a64408ec8
script kiddie <anish.chittu2007@gmail.com>
2025-01-23 22:05:32 +0100

    remove spotify section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,73 +10,24 @@ export default function About() {
                 <div className='w-full select-text flex flex-col border gap-5 border-white border-opacity-15 p-5 text-white rounded-xl'>
                     <h1 className='w-full text-white font-semibold sm:text-2xl'>About</h1>
                     <div className='flex flex-col gap-1'>
-                        <p className='w-full text-white tracking-wide text-opacity-35'>TODAY</p>
-                        <p className='w-full text-white text-opacity-50'>I am a freelance full-stack developer specializing in backend development with Go. On the frontend, I work with Next.js and Tailwind to create clean, functional interfaces. Outside of coding, I explore 3D modeling and enjoy making music, constantly pushing myself to grow creatively and expand my skill set.</p>
+                        <p className='w-full  text-white tracking-wide text-opacity-35'>TODAY</p>
+                        <p className='w-full text-lg text-white text-opacity-50'>I am a freelance full-stack developer specializing in backend development with Go. On the frontend, I work with Next.js and Tailwind to create clean, functional interfaces. Outside of coding, I explore 3D modeling and enjoy making music, constantly pushing myself to grow creatively and expand my skill set.</p>
                     </div>
 
                     <div className='flex flex-col gap-1'>
-                        <p className='w-full text-white tracking-wide text-opacity-35'>CHILDHOOD</p>
-                        <p className='w-full text-white text-opacity-50'>Growing up, I was drawn to trading, gaming, and playing guitar. Trading taught me discipline and how to make thoughtful decisions, starting when I was just 13. At 14, I became interested in programming as I wanted to develop an algorithm to improve my trading system. This sparked my journey into the tech world.</p>
+                        <p className='w-full  text-white tracking-wide text-opacity-35'>CHILDHOOD</p>
+                        <p className='w-full text-lg text-white text-opacity-50'>Growing up, I was drawn to trading, gaming, and playing guitar. Trading taught me discipline and how to make thoughtful decisions, starting when I was just 13. At 14, I became interested in programming as I wanted to develop an algorithm to improve my trading system. This sparked my journey into the tech world.</p>
                     </div>
 
                     <div className='flex flex-col gap-1'>
-                        <p className='w-full text-white tracking-wide text-opacity-35'>GROWTH</p>
-                        <p className='w-full text-white text-opacity-50'>I got into backend development by teaching myself while working on that trading algorithm. The Go programming language really ignited my passion for programming. It introduced me to important concepts like multithreading, memory management, and pointers. What excites me most about backend development is building systems that millions of people use every second. I love optimizing these systems to be as fast as possible, and I have a particular fondness for low-level development.</p>
+                        <p className='w-full  text-white tracking-wide text-opacity-35'>GROWTH</p>
+                        <p className='w-full text-lg text-white text-opacity-50'>I got into backend development by teaching myself while working on that trading algorithm. The Go programming language really ignited my passion for programming. It introduced me to important concepts like multithreading, memory management, and pointers. What excites me most about backend development is building systems that millions of people use every second. I love optimizing these systems to be as fast as possible, and I have a particular fondness for low-level development.</p>
                     </div>
 
                     <div className='flex flex-col gap-1'>
-                        <p className='w-full text-white tracking-wide text-opacity-35'>GOAL</p>
-                        <p className='w-full text-white text-opacity-50'>My ultimate goal is to build my own startup, where I can apply everything I’ve learned and continue growing. I have a unique approach to development: I start by building a basic implementation and then spend hours profiling and optimizing the code. I dive deep into stack traces and enjoy the learning process, knowing that every challenge helps me grow as a developer.</p>
+                        <p className='w-full  text-white tracking-wide text-opacity-35'>GOAL</p>
+                        <p className='w-full text-lg text-white text-opacity-50'>My ultimate goal is to build my own startup, where I can apply everything I’ve learned and continue growing. I have a unique approach to development: I start by building a basic implementation and then spend hours profiling and optimizing the code. I dive deep into stack traces and enjoy the learning process, knowing that every challenge helps me grow as a developer.</p>
                     </div>
-                </div>
-                
-                <div className='w-full justify-evenly  flex-col border border-white border-opacity-15 p-5 text-white rounded-xl'>
-                    <iframe 
-                        className="rounded-md" 
-                        src="https://open.spotify.com/embed/track/3TVWXE8hUeSzoAC3xCrErX?utm_source=generator"
-                        width="100%" 
-                        height="20%" 
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                        loading="lazy">
-                    </iframe>
-
-                    <iframe 
-                        className="rounded-md" 
-                        src="https://open.spotify.com/embed/track/0qGZkkxnyfGrUBLflKwBQG?utm_source=generator"
-                        width="100%" 
-                        height="20%" 
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                        loading="lazy">
-                    </iframe>
-
-                    <iframe 
-                        className="rounded-md" 
-                       src="https://open.spotify.com/embed/track/6PDlkWmrq2ZKiUuFt2aQsH?utm_source=generator" 
-                        width="100%" 
-                        height="20%" 
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                        loading="lazy">
-                    </iframe>
-
-                    <iframe 
-                        className="rounded-md" 
-                        src="https://open.spotify.com/embed/track/3zi9xNpG2WHCdCAxV0BpJm?utm_source=generator"
-                        width="100%" 
-                        height="20%" 
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                        loading="lazy">
-                    </iframe>
-
-                    <iframe 
-                        className="rounded-md" 
-                        src="https://open.spotify.com/embed/track/4DQttwipnILl88cru3BRZx?utm_source=generator"
-                        width="100%" 
-                        height="20%" 
-                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-                        loading="lazy">
-                    </iframe>
-
-                    
                 </div>
             </div>
 
