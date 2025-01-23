7249274620bcbc7d613f28994dc01067a47a5aca
script kiddie <anish.chittu2007@gmail.com>
2025-01-23 21:44:08 +0100

    Enhance About page layout and content; add responsive design for 1080p screens

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,22 +5,85 @@ import { Model } from '@/components/Model'
 
 export default function About() {
     return (
-        <>  
-            <div id="about" className="w-full text-white overflow-hidden flex my-8 text-2xl flex-col items-center gap-5">
-                <h1 className="sm:w-1/2 w-4/5 text-center font-semibold text-white text-opacity-100 sm:text-4xl">BACKGROUND</h1>
-                <p className=" text-white text-opacity-50 sm:w-1/2 w-4/5 text-whitel">
-                Hey! I’m a 17-year-old born in Suriname with a passion for programming, trading, and gaming. What drives me is the motivation to secure my future. I work hard to ensure I can lead a comfortable life later on with a family of five. My greatest strength? Perseverance. I might be average at many things, but when I truly want something, I give it my all to achieve it.
-                </p>
+        <div className='min-h-screen flex flex-col  items-center w-full'>  
+            <div className='sm:w-1/2 w-4/5  h-fit flex 1xl:flex-row 2xl:flex-row flex-col gap-5'> 
+                <div className='w-full select-text flex flex-col border gap-5 border-white border-opacity-15 p-5 text-white rounded-xl'>
+                    <h1 className='w-full text-white font-semibold sm:text-2xl'>About</h1>
+                    <div className='flex flex-col gap-1'>
+                        <p className='w-full text-white tracking-wide text-opacity-35'>TODAY</p>
+                        <p className='w-full text-white text-opacity-50'>I am a freelance full-stack developer specializing in backend development with Go. On the frontend, I work with Next.js and Tailwind to create clean, functional interfaces. Outside of coding, I explore 3D modeling and enjoy making music, constantly pushing myself to grow creatively and expand my skill set.</p>
+                    </div>
 
-                <p className=" text-white text-opacity-50 sm:w-1/2 w-4/5 text-whitel">
-                I chose this program because, during my time in high school, I felt like I wasn’t learning much that truly felt relevant.  
-                My motivation stems from my passion for technology and making a computer do what I want.  
-                I’m particularly drawn to backend development because I enjoy building systems that are efficient and powerful.  
-                After completing this program, I aim to start my own tech company.  
-                To achieve that, I’m honing my skills and diving into the latest backend technologies.  
-                This program is bringing me closer to my goal.
-                </p>
+                    <div className='flex flex-col gap-1'>
+                        <p className='w-full text-white tracking-wide text-opacity-35'>CHILDHOOD</p>
+                        <p className='w-full text-white text-opacity-50'>Growing up, I was drawn to trading, gaming, and playing guitar. Trading taught me discipline and how to make thoughtful decisions, starting when I was just 13. At 14, I became interested in programming as I wanted to develop an algorithm to improve my trading system. This sparked my journey into the tech world.</p>
+                    </div>
+
+                    <div className='flex flex-col gap-1'>
+                        <p className='w-full text-white tracking-wide text-opacity-35'>GROWTH</p>
+                        <p className='w-full text-white text-opacity-50'>I got into backend development by teaching myself while working on that trading algorithm. The Go programming language really ignited my passion for programming. It introduced me to important concepts like multithreading, memory management, and pointers. What excites me most about backend development is building systems that millions of people use every second. I love optimizing these systems to be as fast as possible, and I have a particular fondness for low-level development.</p>
+                    </div>
+
+                    <div className='flex flex-col gap-1'>
+                        <p className='w-full text-white tracking-wide text-opacity-35'>GOAL</p>
+                        <p className='w-full text-white text-opacity-50'>My ultimate goal is to build my own startup, where I can apply everything I’ve learned and continue growing. I have a unique approach to development: I start by building a basic implementation and then spend hours profiling and optimizing the code. I dive deep into stack traces and enjoy the learning process, knowing that every challenge helps me grow as a developer.</p>
+                    </div>
+                </div>
+                
+                <div className='w-full justify-between  flex-col border border-white border-opacity-15 p-5 text-white rounded-xl'>
+                    <iframe 
+                        className="rounded-md" 
+                        src="https://open.spotify.com/embed/track/3TVWXE8hUeSzoAC3xCrErX?utm_source=generator"
+                        width="100%" 
+                        height="20%" 
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                        loading="lazy">
+                    </iframe>
+
+                    <iframe 
+                        className="rounded-md" 
+                        src="https://open.spotify.com/embed/track/0qGZkkxnyfGrUBLflKwBQG?utm_source=generator"
+                        width="100%" 
+                        height="20%" 
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                        loading="lazy">
+                    </iframe>
+
+                    <iframe 
+                        className="rounded-md" 
+                       src="https://open.spotify.com/embed/track/6PDlkWmrq2ZKiUuFt2aQsH?utm_source=generator" 
+                        width="100%" 
+                        height="20%" 
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                        loading="lazy">
+                    </iframe>
+
+                    <iframe 
+                        className="rounded-md" 
+                        src="https://open.spotify.com/embed/track/3zi9xNpG2WHCdCAxV0BpJm?utm_source=generator"
+                        width="100%" 
+                        height="20%" 
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                        loading="lazy">
+                    </iframe>
+
+                    <iframe 
+                        className="rounded-md" 
+                        src="https://open.spotify.com/embed/track/4DQttwipnILl88cru3BRZx?utm_source=generator"
+                        width="100%" 
+                        height="20%" 
+                        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+                        loading="lazy">
+                    </iframe>
+
+                    
+                </div>
             </div>
+
+
+
+
+    
 
             <div id="boxes" className="w-full h-full 2xl:h-screen flex gap-5 flex-col items-center">
                 <div className="sm:w-1/2 border border-white border-opacity-10 w-4/5 h-80 2xl:h-1/2 mt-5 bg-black flex flex-col py-5 rounded-xl">
@@ -28,7 +91,6 @@ export default function About() {
                         <p className="w-full text-white px-5 text-opacity-45">I love tuning and working on cars.</p>
                         <div className="w-full relative h-full py-5 flex justify-center items-center ">
                         <Model />
-                           
                         </div> 
                 </div>
                 
@@ -36,7 +98,7 @@ export default function About() {
                     <div className="w-full overflow-hidden border border-white border-opacity-10 h-80 2xl:h-full p-5 bg-black flex flex-col rounded-xl items-center">
                         <h1 className="w-full text-white font-semibold sm:text-2xl">Photography</h1>
                         <p className="w-full text-white text-opacity-45">One of my recent hobbies.</p>
-                        <div className="w-full h-full p-5 flex justify-center items-center ">
+                        <div className="w-full p-5 flex justify-center items-center ">
                             <img draggable="false" alt="dancing gopher" decoding="async" loading="lazy" src="go.gif" className="rounded-md py-1 hover:scale-110 duration-500" /> {/* 'placeholder' remove this for photos from camera with a filter or sum to make it dark */}
                         </div> 
                     </div>
@@ -47,10 +109,8 @@ export default function About() {
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
 
-
-// I am a freelance full-stack developer specializing in backend development with Go. On the frontend, I work with Next.js and Tailwind to create clean, functional interfaces. Outside of coding, I explore 3D modeling and enjoy making music, constantly pushing myself to grow creatively and expand my skill set.
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        '1080p': '1920px',
+      },
     },
   },
   plugins: [],
